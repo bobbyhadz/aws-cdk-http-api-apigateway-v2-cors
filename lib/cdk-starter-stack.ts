7ec44aaf0bfee337dc3478c08013eba86606096f
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-12 08:40:23 +0300

    updated infrastructure code

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -4,6 +4,7 @@ import * as cdk from '@aws-cdk/core';
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
     const httpApi = new HttpApi(this, 'cors-demo-api', {
       description: 'API for CORS demo',
       corsPreflight: {
@@ -23,6 +24,7 @@ export class CdkStarterStack extends cdk.Stack {
         ],
         allowCredentials: true,
         allowOrigins: ['http://localhost:3000'],
+        // 👇 optionally cache responses to preflight requests
         // maxAge: cdk.Duration.minutes(5),
       },
     });
